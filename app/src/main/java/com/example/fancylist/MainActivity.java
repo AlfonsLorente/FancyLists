1d5335f1ae4332253e6aff39df8efd49c25d7919
Alfons Lorente Darder <alorente@cifpfbmoll.com>
2022-01-28 19:31:14 +0100

    First done - have to do first release.

--- app/src/main/java/com/example/fancylist/MainActivity.java
+++ app/src/main/java/com/example/fancylist/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.fancylist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+// PAGINA 119 LLIBRE Beginning.Android.3File
+
+
+public class MainActivity extends ListActivity {
+
+    private TextView selection;
+    private static final String[] items={"lorem", "ipsum", "dolor",
+            "sit", "amet", "consectetuer", "adipiscing", "elit",
+            "morbi", "vel", "ligula", "vitae", "arcu", "aliquet",
+            "mollis", "etiam", "vel", "erat", "placerat", "ante",
+            "porttitor", "sodales", "pellentesque", "augue", "purus"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this,
+                R.layout.row, R.id.label, items));
+        selection=(TextView)findViewById(R.id.selection);
+    }
+
+    public void onListItemClick(ListView parent, View v,
+                                int position, long id) {
+        selection.setText(items[position]);
     }
 }
